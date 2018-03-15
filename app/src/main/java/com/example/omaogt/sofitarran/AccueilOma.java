d4167cce143c6dc4d45219c9363b5bc22a70b7da
Oma Ogt <omaogt@MacBook-Pro-de-Oma.local>
2018-03-15 16:20:04 +0100

    Ajout des fonctionnalité Page prec et suivante ainsi que résumé

--- app/src/main/java/com/example/omaogt/sofitarran/AccueilOma.java
+++ app/src/main/java/com/example/omaogt/sofitarran/AccueilOma.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,11 +21,28 @@ import android.widget.Toast;
 public class AccueilOma extends AppCompatActivity {
     Button alert;
 
+    TextView txt ;
+    String tex="Année : 2022\n \n" +
+
+            "Pays :  Etats Unis\n \n"+
+
+            "Lieu : Grande Maison bourgeoise sur-protégé.\n \n"+
+
+            "Situation du pays: En 2022, les États-Unis renaissent, gouvernés par les Nouveaux Pères Fondateurs. \n \n Pour maintenir un faible taux de chômage et de criminalité tout au long de l'année, le gouvernement a mis en place une période annuelle de douze heures, au cours de laquelle toute activité criminelle est permise. \n \n Au cours de cette nuit, officiellement appelée « la Purge », chacun peut évacuer ses émotions négatives en réglant ses comptes, ou plus simplement en s'adonnant à la violence gratuite.\n \n"+
+            "Règles : Les seules règles sont l'interdiction d'attaquer tout membre du gouvernement ayant un niveau d’autorisation supérieur à 10 et la prohibition des armes de classe cinq ou de niveau supérieur (matériel explosif tel que les grenades ou les lance-roquettes). \n \n Les contrevenants sont exécutés publiquement par pendaison. \n ";
+
+    ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueiloma);
 
+
+        txt= (TextView) findViewById(R.id.hist1);
+        txt.setText(tex);
+        txt.startAnimation(AnimationUtils.loadAnimation(AccueilOma.this, android.R.anim.slide_in_left));
 
         alert = (Button) findViewById(R.id.jouer);
         alert.setOnClickListener(new View.OnClickListener() {
